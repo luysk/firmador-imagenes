389833f87d5c458f5fb348d7734b9adbee0c93ff
Luis Villagra <luisvillagra0816+1@gmail.com>
2026-06-16 20:50:13 -0600

    Cambio en la plantilla

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,13 +12,15 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
+    "sphinx_wagtail_theme"
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # html_theme = 'sphinx_rtd_theme'
-html_theme = 'alabaster'
+
+html_theme = 'sphinx_wagtail_theme'
 html_static_path = ['_static']
 # html_last_updated_fmt = '%b %d, %Y'
 
